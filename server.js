383cf99463ec996b6694bd65001a488ec9ff0f8f
david <davidwalker2235@gmail.com>
2023-12-20 15:17:47 +0100

    add socket io

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
+
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
