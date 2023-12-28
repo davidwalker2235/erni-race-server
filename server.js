f654928986d8c1d55ecd00fc1d9512a800d6b0c1
david <davidwalker2235@gmail.com>
2023-12-28 09:38:11 +0100

    add cors

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3001;
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {cors: {origin: "*"}});
+
+app.use(cors({
+  origin: '*'
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
