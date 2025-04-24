660ceef25f4b023320e9028d03112f7d0dc87936
David Carmona <davidwalker2235@gmail.com>
2025-04-24 10:10:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
